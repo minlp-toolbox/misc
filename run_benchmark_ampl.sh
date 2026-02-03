feea65d386c883c359ebcf829a2bf30b02c2bc53
aghezz1 <andrea.ghezzi.96@outlook.it>
2026-02-03 20:27:24 +0100

    add shell script to run benchmark

---
+++ run_benchmark_ampl.sh
@@ -0,0 +1,12 @@
+path_to_file=$1
+path_to_output=$2
+
+mkdir -p $path_to_output
+
+python using_amplpy.py cvx gurobi $path_to_file $path_to_output
+python using_amplpy.py cvx scip $path_to_file $path_to_output
+python using_amplpy.py cvx xpress $path_to_file $path_to_output
+
+python using_amplpy.py noncvx gurobi $path_to_file $path_to_output
+python using_amplpy.py noncvx scip $path_to_file $path_to_output
+python using_amplpy.py noncvx xpress $path_to_file $path_to_output
